19c354f17c47bc890e22d81d8af824d3466c2d69
YuanOwO <70084604+hijimmy87@users.noreply.github.com>
2021-12-14 23:18:51 +0800

    灰階

--- js/advanced.js
+++ js/advanced.js
@@ -1,3 +1,7 @@
+onOpenCvReady = () => {
+    $('#loadingOpenCV').remove();
+};
+
 /**
  * submit handler
  */
@@ -21,37 +25,44 @@ function process() {
     let img = $('<img class="w-100">')[0],
         file = $('#img')[0].files[0];
     img.src = URL.createObjectURL(file);
-    let email = $('#email').val();
     img.onload = function() {
         // timer
         let start = new Date(), end;
         console.log(start);
-        let cvs = $('<canvas>')[0],
+        let cvs = $('<canvas class="w-100">')[0],
             ctx = cvs.getContext('2d');
         // image ratio
         if ($('#ratio').prop('checked')) {
             if (width = parseInt($('#width').val())) {
-                cvs.width = width;
-                cvs.height = img.height * width / img.width;
+                let ratio = width / img.width;
+                img.width = width;
+                img.height *= ratio;
             }
             else {
-                cvs.height = parseInt($('#height').val()) || img.height;
-                cvs.width = img.width * cvs.height / img.height;
+                let ratio = (parseInt($('#height').val()) || img.height) / img.height;
+                img.width  *= ratio;
+                img.height *= ratio;
             }
         }
         else {
-            cvs.width  = parseInt($('#width' ).val()) || img.width;
-            cvs.height = parseInt($('#height').val()) || img.height;
+            img.width  ||= parseInt($('#width' ).val());
+            img.height ||= parseInt($('#height').val()) || img.height;
         }
-
-        // gets image pixels data
-        ctx.drawImage(this, 0, 0, cvs.width, cvs.height);
+        // RGB to GrayScale
+        let src = cv.imread(img);
+        if ($('#GrayScale').prop('checked')) {
+            cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
+        }
+        cv.imshow(cvs, src);
+        src.delete();
+        // Pixels Info
         let data = ctx.getImageData(0, 0, cvs.width, cvs.height).data;
         let imgSpace = $('#imgSpace').val();
             space = $('#space').val();
         let coords = [];
         for (let i = 0; i < data.length; i += 4) {
-            let color = new Color(imgSpace, Array.from(data.slice(i, i + 3)).map(x =>x / 255)),
+            let rgb = Array.from(data.slice(i, i + 3))
+            let color = new Color(imgSpace, rgb.map(x => Math.round(x / 51 * 20) / 100)),
                 coord = color.to(space).coords.map(x => Math.round(x * 100) / 100);
             for (let j = 0; j < coord.length; j++) {
                 coord[j] ||= 0;
@@ -59,23 +70,25 @@ function process() {
                 else    coords[j].push(coord[j]);
             }
         }
+        //
+        let outlierPercent = $('#outlier').val();
         let titles = Object.keys(Color.space(space).coords);
+        let email = $('#email').val();
         $('main').empty();
         let $main = $('main'),
             $info = $('<div class="row mb-3">');
-        
         $main.html('<h3>預覽</h3>');
         $main.append($info);
-        $main.append('<button class="btn btn-primary mb-5" type="button" onclick="location.reload()">重新提交</button>')
+        $main.append('<button class="btn btn-primary mb-5" type="button" onclick="location.reload()">重新提交</button>');
         $main.append('<h3>詳細數據</h3>');
         for (let i = 0; i < titles.length; i++)
-            $main.append(detail(titles[i], coords[i]));
+            $main.append(detail(titles[i], coords[i], outlierPercent));
         // timer
         end = new Date();
         console.log(end);
         let delta = new Date(end - start);
         delta = `${delta.getUTCHours()} 時 ${delta.getUTCMinutes()} 分 ${delta.getUTCSeconds()}.${delta.getUTCMilliseconds()} 秒`;
-        $info.append($('<div class="col col-12 col-md-4"></div>').append(img));
+        $info.append($('<div class="col col-12 col-md-4"></div>').append(cvs));
         $info.append(  `<div class="col col-12 col-md-8"><table class="table table-hover">
                         <thead><tr><th scope="col">基本資訊</th><th scope="col">數值</th></tr></thead>
                         <tbody>
@@ -108,12 +121,13 @@ function quantile(array, p) {
 /**
  * Creates the table and the chart
  */
-function detail(title, data) {
+function detail(title, data, percent = 0) {
     const $element = $('<div class="row mb-3">');
     let count = {},
         maxCount = 0,
         modes = [];
     data.sort((a,b) => a - b);
+    data = data.slice(data.length * percent / 200, data.length * (1 - percent / 200));
     // Finds Modes
     for (let value of data) {
         if (count[value]) count[value].y++;
@@ -126,7 +140,6 @@ function detail(title, data) {
             modes.push(value);
         }
     }
-
     // Table
     let $table = $(`<table class="table table-hover">
                     <thead><tr><th scope="col">${title}</th><th scope="col">數值</th></tr></thead>
@@ -145,7 +158,6 @@ function detail(title, data) {
     $tbody.append( `<tr><th scope="row">平均值</th><td>${mean}</td></tr>
                     <tr><th scope="row">標準差</th><td>${sd}</td></tr>`);
     $element.append($('<div class="col col-12 col-sm-6 col-md-4"></div>').html($table));
-
     // Chart
     const $chart = $('<div class="col col-12 col-sm-6 col-md-8"><canvas></canvas></div>'),
           cvs = $chart.children()[0];
